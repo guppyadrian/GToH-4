f6f5b4cace3fd0a86615afa8ddd21a2a360c13c2
guppy <82202242+guppyadrian@users.noreply.github.com>
2025-09-07 21:14:26 -0700

    new camera easing

--- src/scenes/gameScene.ts
+++ src/scenes/gameScene.ts
@@ -22,9 +22,12 @@ export class GameScene extends Scene {
 
     update() {
         this.player.update();
-        Camera.x = this.player.center.x - Master.width / 2
-        Camera.y = this.player.center.y - Master.height / 2; 
+        // Camera.x = this.player.center.x - Master.width / 2
+        // Camera.y = this.player.center.y - Master.height / 2; 
+        Camera.x -= Math.round((Camera.x - (this.player.center.x - Master.width / 2)) / 10 * Master.width) / Master.width; // TODO: Rewrite this
+        Camera.y -= Math.round((Camera.y - (this.player.center.y - Master.height / 2)) / 10 * Master.height) / Master.height;
     }
+
 
     draw() {
         World.draw();
