20ef884b64b16787fb1bafdf03935f79eb641c8a
guppy <82202242+guppyadrian@users.noreply.github.com>
2025-10-26 14:49:24 -0700

    ancient progress

--- src/scenes/ancientGameScene.ts
+++ src/scenes/ancientGameScene.ts
@@ -1,24 +1,32 @@
-import { Canvas, Scene } from "guppy-lib";
+import { Assets, Canvas, Scene } from "guppy-lib";
+import { AncientPlayer } from "../ancientGame/player";
 
 export class AncientGameScene extends Scene {
 
     player: AncientPlayer;
 
     static preload(): Promise<void> {
-        return Promise.resolve();
+        const promises = [];
+
+        promises.push(Assets.load('textures/player.png', 'player'));
+        
+        return Promise.all(promises).then(() => { });
     }
 
     constructor() {
         super();
 
         Canvas.changeResolution(300, 300);
+
+        this.player = new AncientPlayer(150, 60);
     }
 
     update(): void {
+        this.player.update();
         
     }
 
     draw(): void {
-        
+        this.player.draw();
     }
 }
